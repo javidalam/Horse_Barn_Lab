1639bcf4e98eccc6ce066ab437209f2a0fc86202
javidalam <jalam6556@bths.edu>
2018-10-25 10:17:51 -0400

    KYS

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -2,6 +2,11 @@ public class HorseBarn
 {
     private Horse[] spaces;
 
+    public HorseBarn(Horse[] array)
+    {
+        this.spaces = array;
+    }
+
     public void consolidate()
     {
         for (int i = 0; i < this.spaces.length - 1; i++)
@@ -20,5 +25,25 @@ public class HorseBarn
             }
         }
     }
-    public int findHorseSpace()
+    public int findHorseSpace(String name)
+    {
+        for (int i = 0; i < this.spaces.length; i++)
+        {
+            if (this.spaces[i] != null && name.equals(this.spaces[i].getName()))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String toString()
+    {
+        String enter = "";
+        for (int i = 0; i < spaces.length; i++)
+        {
+            enter += "[" + spaces[i] + "]";
+        }
+        return enter;
+    }
 }
